3539a0d63b6c5d9bf2e34c422297fdd3a3c38abd
MRodriguezAguilar <marijack1997@gmail.com>
2023-07-16 11:40:00 -0500

    Curso preparatorio

---
+++ funciones2.js
@@ -0,0 +1,13 @@
+function cuidadoConElConsoleLog(nombre) {
+    console.log(nombre);
+  } 
+
+  function otraFuncion() {
+    return ( "El nombe retornado por la funcion 'cuidadoConElConsoleLog' es: " + 
+    cuidadoConElConsoleLog('camilo')
+    );
+  }
+
+ 
+    
+    
